aacfd9dd9fd08433ad8ad676aeb9fef3dba1b6db
Carlos.migal <carlosmigria@gmail.com>
2024-11-13 14:34:02 +0100

    TAREAS BASES DE DATOS

--- Pyithon/listas/8.py
+++ Pyithon/listas/8.py
@@ -2,14 +2,14 @@
 from decimal import Decimal as D
 
 def introduceDatos(listaalumno):
-    for i in range(2):  # Cambia 2 por el número de alumnos que desees
+    for i in range(2):
         lista = []
         Alumno = input("Alumno: ")
         Nombre = input("Nombre: ")
         lista.append((Alumno, Nombre))
         
         total_notas = D(0)
-        num_asignaturas = 2  # Cambia a la cantidad de asignaturas por alumno
+        num_asignaturas = 2 
         
         for x in range(num_asignaturas):
             Asignatura = input("Asignatura: ")
@@ -18,13 +18,12 @@ def introduceDatos(listaalumno):
             total_notas += nota
         
         media = total_notas / num_asignaturas
-        lista.append(("Media", media))  # Añadimos la media de cada alumno
+        lista.append(("Media", media))  
         listaalumno.append(lista)
     
     return listaalumno
 
 def ordenado(milista):
-    # Ordenamos la lista por el nombre del alumno (index [0][1])
     milistaordenada = sorted(milista, key=lambda x: x[0][1])
     return milistaordenada
 
@@ -34,12 +33,12 @@ def mostrar(milistaordenada):
     for alumno in milistaordenada:
         print("\nAlumno:", alumno[0][0])
         print("Nombre:", alumno[0][1])
-        for asignatura, nota in alumno[1:-1]:  # Excluye la última entrada (media)
+        for asignatura, nota in alumno[1:-1]:
             print(asignatura, "-----", nota)
-        print("Media del Alumno:", alumno[-1][1])  # La última entrada es la media
+        print("Media del Alumno:", alumno[-1][1])
 
 def calcular_media_general(milista):
-    total_media = sum(alumno[-1][1] for alumno in milista)  # Suma las medias de cada alumno
+    total_media = sum(alumno[-1][1] for alumno in milista)
     media_general = total_media / len(milista)
     return media_general
 
@@ -48,9 +47,8 @@ def main(args):
     milista = introduceDatos(milista)
     milistaordenada = ordenado(milista)
     mostrar(milistaordenada)
-    
     media_general = calcular_media_general(milista)
-    print("\nMedia General de todos los alumnos:", media_general)
+    print("Media General de todos los alumnos:", media_general)
     
     return 0
 
