1f054f988dba87bff93ea3158238397c7571820f
Carlos.migal <carlosmigria@gmail.com>
2024-12-12 14:54:20 +0100

    php

--- Pyithon/Diccionarios/1.py
+++ Pyithon/Diccionarios/1.py
@@ -11,37 +11,23 @@ def mostrarDatos(diccionario):
                 for p in valor:
                     print(f"\t{p}")
 def introducirDatos():
-    fran={}
+    Bdatos1={}
     s = int(input("Intoduce el numero de servidores que quieras dar de alta:"))
     for i in range(s):
         NServer=input("Introduce el nombre del servidor:")
         ip=input("Introduce la ip del servidor:")
         masc=input("Introduce la masc servidor:")
-        gatewayu=input("Introduce la masc servidor:")
+        gateway=input("Introduce la gateway servidor:")
         NDNS=int(input("Introduce el numero de DNS que quieres ponerle al servidor:"))
         for i in range(NDNS):
             DNS=input("Introduce el nombre del servidor:")
-            fran.update({NServer:{"ip:": ip,"masc: ": masc,"DNS: ": DNS,}})
-    return fran
+            Bdatos1.update({NServer:{"ip:": ip,"masc: ": masc,"gateway:": gateway,"DNS: ": DNS,}})
+    return Bdatos1
 
 def main(args):
     
-    datos= {
-           'Serv1':{
-                   "ip":"192.168.8.1",
-                   "masc":"255.255.255.0",
-                   "gateway":"192.168.8.1",
-                   "DNS":["1.1.1.1","8.8.8.8"]
-                   },
-           'Serv2':{
-                   "ip":"192.168.8.2",
-                   "masc":"255.255.255.0",
-                   "gateway":"192.168.8.1",
-                   "DNS":["1.1.1.1","8.8.8.8"]
-                   }
-            }
-    fran=introducirDatos()
-    print(fran)
+    datos=introducirDatos()
+    print(datos)
     return 0
 
 if __name__ == '__main__':
