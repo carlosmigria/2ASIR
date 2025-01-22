aec8d6b991cff3953cc4921279813a8bc6a2a450
Migal RC <mirica_aliesma20@iesmedinaazahara.es>
2025-01-22 16:28:50 +0100

    a

---
+++ Pyithon/listas/pre examen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+def introducirL(milista)
+    milista=(nombre,apellido1,apellido2)
+    return milista
+def main(args):
+    nombre=input("Introduce el nombre del usuario:")
+    apelleido1=input("Introduce el nombre del usuario:")
+    apellido2=input("Introduce el nombre del usuario:")
+    contador = 0
+    milista()
+    print (milista)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
